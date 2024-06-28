f8f9ee929434162792a9d2955d76c578f698616b
sauravvr <sauravgaur@valueresearch.in>
2024-06-29 00:42:22 +0530

    made member sides private

--- src/utils/dice.util.js
+++ src/utils/dice.util.js
@@ -2,7 +2,7 @@ const commonConfig = require('../config/common.config');
 
 class Dice {
 
-    sides = commonConfig.DEFAULT_DICE_SIDES;
+    #sides = commonConfig.DEFAULT_DICE_SIDES;
 
     static roll() {
         return (Math.floor(Math.random() * sides) + 1);
