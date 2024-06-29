751bc2bfc7405c041240934d6e4cb854f4283011
sauravvr <sauravgaur@valueresearch.in>
2024-06-29 15:41:36 +0530

    misc

--- src/models/magicalArena.model.js
+++ src/models/magicalArena.model.js
@@ -1,6 +1,7 @@
 const Player = require('./player.model.js');
 const Dice = require('../utils/dice.util.js');
 const Future = require('../services/future.service.js');
+const Logger = require('../services/loger/loger.service.js');
 
 class MagicalArena {
 
@@ -16,35 +17,35 @@ class MagicalArena {
             ? [playerA, playerB]
             : [playerB, playerA]
 
-        console.log(`\n\n\nMatch Started : ${attacker.name} 🗡️  ${defender.name}\n\n\n`);
+        Logger.log(`\n\n\nMatch Started : ${attacker.name} 🗡️  ${defender.name}\n\n\n`);
 
 
         while(attacker.isAlive && defender.isAlive) {
-            console.log(`Attacker : ${attacker.name} is rolling... 🎲`);
+            Logger.log(`Attacker : ${attacker.name} is rolling... 🎲`);
             await Future.delay(500);
             let _boosterDice = Dice.roll();
-            console.log(`Attacker : ${attacker.name} Got the Booster as: ${_boosterDice}`);
+            Logger.log(`Attacker : ${attacker.name} Got the Booster as: ${_boosterDice}`);
             const attackerDamage = attacker.calcDamage(_boosterDice);
-            console.log(`Defender : ${defender.name} is rolling... 🎲`);
+            Logger.log(`Defender : ${defender.name} is rolling... 🎲`);
             await Future.delay(500);
             _boosterDice = Dice.roll();
-            console.log(`Defender : ${defender.name} Got the Booster as: ${_boosterDice}`);
+            Logger.log(`Defender : ${defender.name} Got the Booster as: ${_boosterDice}`);
             const defenderStrength = defender.calcDefence(_boosterDice);
 
             const actualDamage = Math.max((attackerDamage - defenderStrength), 0);
 
             if(actualDamage == 0)
-                console.log(`Defender : ${defender.name} defended this attack 🛡️`);
+                Logger.log(`Defender : ${defender.name} defended this attack 🛡️`);
             else
-                console.log(`Attacker : ${attacker.name} attacked with the damage of : ${actualDamage} 🗡️`);
+                Logger.log(`Attacker : ${attacker.name} attacked with the damage of : ${actualDamage} 🗡️`);
 
 
             defender.takeDamage(actualDamage);
-            console.log(`Remaning Health : ${attacker.name} : ${attacker.health} AND ${defender.name} : ${defender.health}`);
+            Logger.log(`Remaning Health : ${attacker.name} : ${attacker.health} AND ${defender.name} : ${defender.health}`);
 
             [attacker, defender] = this.#toggle(attacker, defender);
 
-            console.log("======================================================================")
+            Logger.log("======================================================================")
         }
 
         this.#announceWinner(attacker, defender);
@@ -56,14 +57,14 @@ class MagicalArena {
     }
 
     #announceWinner(_playerA, _playerB) {
-        console.log('\n');
+        Logger.log('\n');
         if(_playerA.isAlive)
-            console.log(`🏆 ${_playerA.name} 🏆 Won this Match and defeted ${_playerB.name} with ${_playerA.health} health remaning`);
+            Logger.log(`🏆 ${_playerA.name} 🏆 Won this Match and defeted ${_playerB.name} with ${_playerA.health} health remaning`);
         else
-            console.log(`🏆 ${_playerB.name} 🏆 Won this Match and defeted ${_playerA.name} with ${_playerB.health} health remaning`);
+            Logger.log(`🏆 ${_playerB.name} 🏆 Won this Match and defeted ${_playerA.name} with ${_playerB.health} health remaning`);
 
-        console.log('\n')
-        console.log("======================================================================")
+        Logger.log('\n')
+        Logger.log("======================================================================")
     }
 }
 
